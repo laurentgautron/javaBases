01166df5ab8b1228a79faa3d4aa44ccd1a0ab69c
laurent GAUTRON <laurentgautron@protonmail.com>
2022-07-05 12:19:59 +0200

    add Datatypes

---
+++ src/Datatypes.java
@@ -0,0 +1,20 @@
+public class Datatypes {
+    public static void main(String[] args) {
+
+        // chaînes de caractères
+        String prenom = "Laurent";
+        String nom = "GAUTRON";
+
+        // caractères
+        char genre = 'm';
+        // Réels: rajouter un F ou f pour que java accepte le float, sinon il considère que c'est un double
+        float poids = 85.5F;
+        float tjm = 8.4F;
+
+        // Réels double précision
+        double v = 45.3;
+
+        // Byte : une variante d'entier court
+        byte age = 89;
+    }
+}
