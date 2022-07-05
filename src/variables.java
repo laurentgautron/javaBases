3cad0c431541d60aa9492e0a6559d9475252f528
laurent GAUTRON <laurentgautron@protonmail.com>
2022-07-05 11:31:16 +0200

    java bases: variables

---
+++ src/variables.java
@@ -0,0 +1,14 @@
+public class variables {
+    public static void main(String[] args) {
+        int longueur = 200;
+        int largeur = 50;
+
+        int perimetre = 2 * (longueur + largeur);
+        int surface = longueur * largeur;
+        System.out.println(perimetre);
+        System.out.println(surface);
+
+        longueur = 30;
+        largeur = 20;
+    }
+}
