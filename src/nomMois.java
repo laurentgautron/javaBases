686b85787234d687fb5916feef772c7210eb4eda
laurent GAUTRON <laurentgautron@protonmail.com>
2022-07-06 11:05:34 +0200

    modify moi -> mois

--- src/nomMois.java
+++ src/nomMois.java
@@ -4,48 +4,48 @@ public class nomMois {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Entrer un nombre entre 1 et 12 : ");
-        int moi = input.nextInt();
+        int mois = input.nextInt();
 
 
-        switch (moi) {
+        switch (mois) {
             case 1 :
-                System.out.println("Moi de Janvier");
+                System.out.println("Mois de Janvier");
                 break;
             case 2 :
-                System.out.println("Moi de Février");
+                System.out.println("Mois de Février");
                 break;
             case 3 :
-                System.out.println("Moi de Mars");
+                System.out.println("Mois de Mars");
                 break;
             case 4 :
-                System.out.println("Moi d'Avril");
+                System.out.println("Mois d'Avril");
                 break;
             case 5 :
-                System.out.println("Moi de Mai");
+                System.out.println("Mois de Mai");
                 break;
             case 6 :
-                System.out.println("Moi de Juin");
+                System.out.println("Mois de Juin");
                 break;
             case 7 :
-                System.out.println("Moi de Juillet");
+                System.out.println("Mois de Juillet");
                 break;
             case 8 :
-                System.out.println("Moi d'Août");
+                System.out.println("Mois d'Août");
                 break;
             case 9 :
-                System.out.println("Moi de Septembre");
+                System.out.println("Mois de Septembre");
                 break;
             case 10 :
-                System.out.println("Moi de Octobre");
+                System.out.println("Mois de Octobre");
                 break;
             case 11 :
-                System.out.println("Moi de Novembre");
+                System.out.println("Mois de Novembre");
                 break;
             case 12 :
-                System.out.println("Moi de Décembre");
+                System.out.println("Mois de Décembre");
                 break;
             default:
-                System.out.println("Ce moi n'existe pas");
+                System.out.println("Ce mois n'existe pas");
         }
 
     }
