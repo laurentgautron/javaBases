55fd7b9519e92438a47b9a90618ffe72a0ed82e6
laurent GAUTRON <laurentgautron@protonmail.com>
2022-07-06 11:08:26 +0200

    modify equation

--- src/Equation.java
+++ src/Equation.java
@@ -11,18 +11,20 @@ public class Equation {
         double c = input.nextDouble();
 
         double delta = Math.pow(b, 2) - 4 * a * c;
-
-        if (delta < 0) {
-            System.out.println("Pas de solution réelles pour l'équation");
-        } else if ( delta > 0) {
-            System.out.println("Il y a deux solutions:");
-            double x1 = (-b - Math.sqrt(delta))/ (2 * a);
-            double x2 = (-b + Math.sqrt(delta))/ (2 * a);
-            System.out.println("solution 1: " + x1);
-            System.out.println("solution 2: " + x2);
-        } else {
-            double x = -b / ( 2 * a);
-            System.out.print("Une seule solution: " + x);
+        // si a = 0 ce n'est pas une équation du second degré
+        if ( a != 0) {
+            if (delta < 0) {
+                System.out.println("Pas de solution réelles pour l'équation");
+            } else if ( delta > 0) {
+                System.out.println("Il y a deux solutions:");
+                double x1 = (-b - Math.sqrt(delta))/ (2 * a);
+                double x2 = (-b + Math.sqrt(delta))/ (2 * a);
+                System.out.println("solution 1: " + x1);
+                System.out.println("solution 2: " + x2);
+            } else {
+                double x = -b / ( 2 * a);
+                System.out.print("Une seule solution: " + x);
+            }
         }
     }
 }
